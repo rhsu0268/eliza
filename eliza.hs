e4e1adc0bfb629514ab94d0278cc98e0b2594bca
Richard Hsu <rhsu@gwu.edu>
2016-02-28 22:36:12 -0500

    Able to call functions outside of main

--- eliza.hs
+++ eliza.hs
@@ -33,18 +33,34 @@ keywords = [
 	"CAR", 
 	"NOKEYFOUND"]
 
+--action :: IO String
+--action :: [Int] -> [Int]
 
+addThree :: Int -> Int -> Int -> Int
+addThree x y z = x + y + z
+
+getLength :: () -> Int
+getLength () = length keywords
 
 main = do
 	putStrLn "HI! I'M ELIZA. WHAT'S YOUR PROBLEM?"
 	input <- getLine
 	putStrLn input
 
+
 	--check the string
 	if input == "bye"
-		then putStrLn "Thanks for visiting me! Goodbye!"
+		then do
+			putStrLn "Thanks for visiting me! Goodbye!"
 		else do
 			putStrLn "How do you do?"
 			--show keywords
+	--putStrLn result
+	print (getLength ())
+	putStrLn "---End---"
+
+
+
+	
 
 
